6e7040da61d2e2b2fe6c6ed3f173c50a7907e9c1
Matthew Baker <Matthew.d.baker20@gmail.com>
2020-10-06 17:01:59 -0500

    added narcissistic num problem and finished question 1: sum of a sequence of numbers by a step

--- JS/extra_questions_not_edabit.js
+++ JS/extra_questions_not_edabit.js
@@ -26,9 +26,28 @@ function narcissisticNum(j) {
 
 
 
+// Write a function that returns the SUM of a sequence of numbers. This sequence is determined by three variables:
+// start, finish, and step. The sequence begins at start, ends at finish, and goes up by step until it reaches finish
+// or a number that when increased by step would go past finish.
+//
+//     Examples:
+//
+//
+// Input: start: 1 finish: 4 step: 1 | Output: 10
+// Input: start: 4 finish: 10 step: 5| Output: 13
 
 
+function sumOfSequenceSteps(start, finish, step){
+    let sum = 0;
+    for(let i = start; i <= finish; i+=step){
+        sum += i;
+    }
+    return sum;
+}
 
+
+console.log(sumOfSequenceSteps(1, 4, 1));
+console.log(sumOfSequenceSteps(4, 10, 5));
 
 
 
